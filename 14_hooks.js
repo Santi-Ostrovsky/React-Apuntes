b604edaa7c3b6e6b7681411a7fef9280be77c0cc
Santiago Ostrovsky <s.ostrovsky@hotmail.com>
2022-07-10 20:34:06 -0300

    Add files via upload

---
+++ 14_hooks.js
@@ -0,0 +1,16 @@
+/*
+HOOKS
+
+Los Hooks permiten usar estados y otras características de React en componentes funcionales en lugar de componentes de clase.
+
+Syntax --> 'use' + [hook]
+
+Tipos de hooks:
+    - State
+    - Effect
+    - Context
+    - Ref
+    - Reducer
+    - Callback
+    - Personalizados
+*/
